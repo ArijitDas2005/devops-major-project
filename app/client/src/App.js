22a664760c55f08ebc011a05928d6e0b02deeca2
Arijit <rumadas.r123@gmail.com>
2025-07-06 10:47:04 +0530

    Initial commit

---
+++ app/client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>🚀 DevOps Project: Frontend</h1>
+    </div>
+  );
+}
+
+export default App;
+// This is the main App component for the React application.
+// It serves as the entry point for the frontend of the DevOps project.
